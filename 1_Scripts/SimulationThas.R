5a073c836cc590e44a85de2c02a830ef247b3861
HBossier <bossier.han@gmail.com>
2017-08-11 13:03:58 +0200

    checking range of predictor and alpha in simulations

--- 1_Scripts/SimulationThas.R
+++ 1_Scripts/SimulationThas.R
@@ -328,6 +328,249 @@ ReplResults <- data.frame(combinations, AvBetaHat = round(colMeans(BetaValues), 
 
 
 
+##
+##########
+### Running weird models
+##########
+##
+
+##### PART A: range of predictor
+# I would assume that the true value of beta does not depend on the range of the predictor.
+# Is this true?
+
+alpha <- c(1)
+sigma <- c(1)
+u <- seq(1,30,5)
+n <- c(50)
+
+combinations <- expand.grid('alpha' = alpha,'sigma' = sigma,'u' = u, 'n' = n)
+BetaValues <- array(NA, dim = c(nsim, dim(combinations)[1]))
+
+# loop over the combinations: predictor not generated within simulation
+for(c in 1:dim(combinations)[1]){
+  # Print status
+  print(paste0('@: ', combinations[c,]))
+  # Set the parameters
+  nSim <- combinations[c,'n']
+  uSim <- combinations[c,'u']
+  alphaSim <- combinations[c,'alpha']
+  sigmaSim <- combinations[c,'sigma']
+
+  # Generate predictor
+  X <- runif(n = nSim, min = 0.1, max = uSim)
+
+  # Fit the model nsim times
+  for(i in 1:nsim){
+    # Generate data
+    Y <- alphaSim*X + rnorm(n = nSim, mean = 0, sd = sigmaSim)
+
+    # PIM package beta parameter
+    value <- try(pim(formula = Y ~ X, link = 'probit', model = 'difference')@coef, silent = TRUE)
+    if(class(value) == 'try-error'){
+      print(paste0('Error in sim ',i, ' c = ', c, '. Message = ', attr(value,"condition")))
+      next
+    }else{
+      BetaValues[i,c] <- value
+    }
+  }
+}
+
+combinations <- combinations %>% mutate(beta = round(alpha/(sqrt(2) * sigma), digits = 3))
+ReplResults <- data.frame(combinations, AvBetaHat = round(colMeans(BetaValues), digits = 5),
+              VarBetaHat = round(apply(BetaValues, 2, var), digits = 5))
+
+ggplot(ReplResults, aes(x = u, y = AvBetaHat)) + geom_line(aes(colour = '#5ab4ac'), size = 1.5) +
+  geom_line(aes(x = u, y = beta, colour = '#d8b365'), size = 1.5) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "range of predictor") +
+  scale_colour_manual(name = "", values = c('#5ab4ac' = '#5ab4ac', '#d8b365' = '#d8b365'), labels = c('#5ab4ac' = "estimated", '#d8b365' = "true value")) +
+  ggtitle("Increasing range of one generated predictor")
+
+#####
+# generate predictor within simulation for loop
+####
+
+BetaValues <- LMBetaValues <- array(NA, dim = c(nsim, dim(combinations)[1]))
+
+# loop over the combinations: predictor not generated within simulation
+for(c in 1:dim(combinations)[1]){
+  # Print status
+  print(paste0('@: ', combinations[c,]))
+  # Set the parameters
+  nSim <- combinations[c,'n']
+  uSim <- combinations[c,'u']
+  alphaSim <- combinations[c,'alpha']
+  sigmaSim <- combinations[c,'sigma']
+
+  # Fit the model nsim times
+  for(i in 1:nsim){
+    # Generate predictor
+    X <- runif(n = nSim, min = 0.1, max = uSim)
+
+    # Generate data
+    Y <- alphaSim*X + rnorm(n = nSim, mean = 0, sd = sigmaSim)
+
+    # Linear regression beta parameter
+    LMBetaValues[i,c] <- coef(lm(Y ~ X))[2]
+
+    # PIM package beta parameter
+    value <- try(pim(formula = Y ~ X, link = 'probit', model = 'difference')@coef, silent = TRUE)
+    if(class(value) == 'try-error'){
+      print(paste0('Error in sim ',i, ' c = ', c, '. Message = ', attr(value,"condition")))
+      next
+    }else{
+      BetaValues[i,c] <- value
+    }
+  }
+}
+
+combinations <- combinations %>% mutate(beta = round(alpha/(sqrt(2) * sigma), digits = 3))
+ReplResults <- data.frame(combinations, AvBetaHat = round(colMeans(BetaValues, na.rm = TRUE), digits = 5),
+              VarBetaHat = round(apply(BetaValues, 2, var, na.rm = TRUE), digits = 5))
+
+plotPIM <- ggplot(ReplResults, aes(x = u, y = AvBetaHat)) + geom_line(aes(colour = '#5ab4ac'), size = 1.5) +
+  geom_line(aes(x = u, y = beta, colour = '#d8b365'), size = 1.5) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "range of predictor") +
+  scale_colour_manual(name = "", values = c('#5ab4ac' = '#5ab4ac', '#d8b365' = '#d8b365'), labels = c('#5ab4ac' = "estimated", '#d8b365' = "true value")) +
+  ggtitle("Increasing range of predictor (u)")
+plotPIM
+
+LMresults <- data.frame(combinations, AvLMBetaHat = round(colMeans(LMBetaValues, na.rm = TRUE), digits = 5),
+              VarBetaHat = round(apply(LMBetaValues, 2, var, na.rm = TRUE), digits = 5))
+plotLM <- ggplot(LMresults, aes(x = u, y = AvLMBetaHat)) + geom_line(aes(colour = '#6a51a3'), size = 1.5) +
+  geom_line(aes(x = u, y = alpha, colour = '#cc4c02'), size = 1.5) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "range of predictor") +
+  scale_colour_manual(name = "", values = c('#6a51a3' = '#6a51a3', '#cc4c02' = '#cc4c02'), labels = c('#cc4c02' = "true value",'#6a51a3' = "estimated")) +
+  ggtitle("Increasing range of predictor (u)")
+plotLM
+
+cowplot::plot_grid(plotPIM, plotLM, labels = c("PIM: ", "LM: "), ncol = 2, hjust = 0)
+
+RawData <- data.frame('values' = c(matrix(BetaValues, ncol = 1),
+        matrix(LMBetaValues, ncol = 1)),
+        'u' = rep(rep(as.numeric(unlist(combinations['u'])), each = nsim),2),
+        'truth' = c(rep(as.numeric(unlist(combinations['beta'])), each = nsim),
+                    rep(as.numeric(unlist(combinations['alpha'])), each = nsim)),
+        'type' = rep(c("PIM", "LinearRegression"), each = nsim * dim(combinations)[1])) %>% tbl_df()
+
+ggplot(RawData, aes(x = u, y = values)) + geom_smooth(method = 'loess', aes(colour = '#6a51a3')) +
+geom_line(aes(x = u, y = truth, colour = '#cc4c02'), size = 1.5) +
+geom_point(size = 0.8, colour = '#9e9ac8', alpha = 0.2) +
+scale_colour_manual(name = "", values = c('#6a51a3' = '#6a51a3', '#cc4c02' = '#cc4c02'),
+              labels = c('#cc4c02' = "true value",'#6a51a3' = "estimated")) +
+  facet_wrap(~ type) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "range of predictor") +
+    ggtitle("Increasing range of predictor (u)")
 
 
+##### PART B: increasing true value
+alpha <- seq(1,30,5)
+sigma <- c(1)
+u <- c(1)
+n <- c(50)
+
+combinations <- expand.grid('alpha' = alpha,'sigma' = sigma,'u' = u, 'n' = n)
+BetaValues <- LMBetaValues <- array(NA, dim = c(nsim, dim(combinations)[1]))
+
+# loop over the combinations: predictor not generated within simulation
+for(c in 1:dim(combinations)[1]){
+  # Print status
+  print(paste0('@: ', combinations[c,]))
+  # Set the parameters
+  nSim <- combinations[c,'n']
+  uSim <- combinations[c,'u']
+  alphaSim <- combinations[c,'alpha']
+  sigmaSim <- combinations[c,'sigma']
+
+  # Fit the model nsim times
+  for(i in 1:nsim){
+    # Generate predictor
+    X <- runif(n = nSim, min = 0.1, max = uSim)
+
+    # Generate data
+    Y <- alphaSim*X + rnorm(n = nSim, mean = 0, sd = sigmaSim)
+
+    # Linear regression beta parameter
+    LMBetaValues[i,c] <- coef(lm(Y ~ X))[2]
+
+    # PIM package beta parameter
+    value <- try(pim(formula = Y ~ X, link = 'probit', model = 'difference')@coef, silent = TRUE)
+    if(class(value) == 'try-error'){
+      print(paste0('Error in sim ',i, ' c = ', c, '. Message = ', attr(value,"condition")))
+      next
+    }else{
+      BetaValues[i,c] <- value
+    }
+  }
+}
+
+combinations <- combinations %>% mutate(beta = round(alpha/(sqrt(2) * sigma), digits = 3))
+ReplResults <- data.frame(combinations, AvBetaHat = round(colMeans(BetaValues, na.rm = TRUE), digits = 5),
+              VarBetaHat = round(apply(BetaValues, 2, var, na.rm = TRUE), digits = 5))
+
+plotPIM <- ggplot(ReplResults, aes(x = alpha, y = AvBetaHat)) + geom_line(aes(colour = '#5ab4ac'), size = 1.5) +
+  geom_line(aes(x = alpha, y = beta, colour = '#d8b365'), size = 1.5) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "true value") +
+  scale_colour_manual(name = "", values = c('#5ab4ac' = '#5ab4ac', '#d8b365' = '#d8b365'), labels = c('#5ab4ac' = "estimated", '#d8b365' = "true value")) +
+  ggtitle("Increasing true value (alpha)")
+plotPIM
+
+LMresults <- data.frame(combinations, AvLMBetaHat = round(colMeans(LMBetaValues, na.rm = TRUE), digits = 5),
+              VarBetaHat = round(apply(LMBetaValues, 2, var, na.rm = TRUE), digits = 5))
+
+plotLM <- ggplot(LMresults, aes(x = alpha, y = AvLMBetaHat)) + geom_line(aes(colour = '#6a51a3'), size = 1.5) +
+  geom_line(aes(x = alpha, y = alpha, colour = '#cc4c02'), size = 1.5) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "true value") +
+  scale_colour_manual(name = "", values = c('#6a51a3' = '#6a51a3', '#cc4c02' = '#cc4c02'), labels = c('#cc4c02' = "true value",'#6a51a3' = "estimated")) +
+  ggtitle("Increasing true value (alpha)")
+plotLM
+
+cowplot::plot_grid(plotPIM, plotLM, labels = c("PIM: ", "LM: "), ncol = 2, hjust = 0)
+
+RawData <- data.frame('values' = c(matrix(BetaValues, ncol = 1),
+        matrix(LMBetaValues, ncol = 1)),
+        'alpha' = rep(rep(as.numeric(unlist(combinations['alpha'])), each = nsim),2),
+        'truth' = c(rep(as.numeric(unlist(combinations['beta'])), each = nsim),
+                    rep(as.numeric(unlist(combinations['alpha'])), each = nsim)),
+        'type' = rep(c("PIM", "LinearRegression"), each = nsim * dim(combinations)[1]))
+
+ggplot(RawData, aes(x = alpha, y = values)) + geom_smooth(method = 'loess', aes(colour = '#6a51a3')) +
+geom_line(aes(x = alpha, y = alpha, colour = '#cc4c02'), size = 1.5) +
+geom_point(size = 0.8, colour = '#9e9ac8', alpha = 0.2) +
+scale_colour_manual(name = "", values = c('#6a51a3' = '#6a51a3', '#cc4c02' = '#cc4c02'),
+        labels = c('#cc4c02' = "true value",'#6a51a3' = "estimated")) +
+  facet_wrap(~ type) +
+  scale_y_continuous(name = "beta (estimated/true)") +
+  scale_x_continuous(name = "true value") +
+    ggtitle("Increasing true value (alpha)")
+
+
+
+
+
+
+
+## TEST
+
+n <- 200
+u <- 1
+alpha <- 1
+sigma <- 1
+
+# Step one: generate data
+X <- runif(n = n, min = 0.1, max = u)
+Y <- alpha*X + rnorm(n = n, mean = 0, sd = sigma)
+TrueBeta <- alpha/(sqrt(2) * sigma)
+
+# We can first try using the PIM package
+PIMfit <- pim(formula = Y ~ X, data = data.frame(Y = Y, X = X), link = 'probit', model = 'difference')
+summary(PIMfit)
+PIMfit@coef
+
+PIMlogit <- pim(formula = Y ~ X, data = data.frame(Y = Y, X = X), link = 'logit', model = 'difference')
 
